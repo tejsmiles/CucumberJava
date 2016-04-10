be33d588eabfaa7447a42d23368c91d880093050
Tejaswinee Havaldar <tejaswinee@gmail.com>
2016-04-10 21:56:29 +0200

    Formatted reports configuration updated dependencies, bddRunner and README

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ Eclipse maven project with configured to include selenium jar.
 ##Usage
 
 bddRunner Class is the initiation point and is executed as JUnit test
+
+#Test Report
+
+target/reports/final contains reports in html and pdf formats
+
+Reports generated are:
+* Results Coverage Report (html and pdf)
+* Feature Overview Report (html and pdf)
+* Test Results Report (html and pdf)
+
+#Screenshots
+
+final screenshots are stored in the folder target/reports/final/target
+
--- src/test/java/com/goeuro/bddAutomation/bddRunner.java
+++ src/test/java/com/goeuro/bddAutomation/bddRunner.java
@@ -5,11 +5,34 @@ import cucumber.api.junit.*;
 
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(jsonReport = "target/reports/cucumber.json",
+        retryCount = 1,
+        detailedReport = true,
+        detailedAggregatedReport = true,
+        overviewReport = true,
+        coverageReport = true,
+        usageReport = false,
+        toPDF = true,
+        screenShotLocation= "target/reports",
+        outputFolder = "target/reports/final/")
 @CucumberOptions(
-		 features="src/test/resources",
-	     plugin = {"pretty", "html:target/reports/cucumber"}
-	)
+		strict = false, 
+		features="src/test/resources",
+		plugin = { "pretty",
+				"html:target/reports/cucumber",
+        		"json:target/reports/cucumber.json", 
+				"pretty:target/reports/cucumber-pretty.txt",
+			    "usage:target/reports/cucumber-usage.json", 
+			    "junit:target/reports/cucumber-results.xml"}, 
+        tags = { "~@ignore" })
 public class bddRunner {
 
 }
